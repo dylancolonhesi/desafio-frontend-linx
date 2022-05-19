a16e1cfc7e45f88091cd244fa9b08b9c9bfc2113
dylancolonhesi <dylan.colonhesi@bandtec.com.br>
2022-05-19 13:38:39 -0300

    feat: add page details

---
+++ front-end/src/pages/Details.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import Header from '../components/Header/Header';
+import CardDetails from '../components/CardDetails/CardDetails'
+
+import api from '../api';
+import Footer from '../components/Footer/Footer';
+import { useParams } from 'react-router-dom';
+
+function Details() {
+
+    const [brewery, setBrewery] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        async function fetchBreweries() {
+            const resposta = await api.get("/madtree-brewing-cincinnati");
+            setBrewery(resposta.data);
+        }
+        fetchBreweries();
+    }, []);
+
+    return (
+        <>
+            <Header />
+            <CardDetails
+                id={brewery.id}
+                type={brewery.brewery_type}
+                name={brewery.name}
+                street={brewery.street}
+                city={brewery.city}
+                state={brewery.state}
+                postal_code={brewery.postal_code}
+                country={brewery.country}
+                website={brewery.website_url}
+                phone={brewery.phone}
+            />
+            <Footer />
+        </>
+    );
+}
+export default Details;
